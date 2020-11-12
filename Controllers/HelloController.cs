a383677c735ff40acf9c2c57e84f78e9c7a5d66b
Ryan Chaffee <rchaff7@yahoo.com>
2020-11-12 00:46:14 -0600

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        // GET: /<controller>/<action>/<optional parameter>
+        // [HttpGet]
+        // [Route("/helloworld/welcome/{name?}")]
+
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
+        public IActionResult Welcome(string name="World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
