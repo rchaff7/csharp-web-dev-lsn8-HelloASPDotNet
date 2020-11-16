04c8490866e9a1bc1927086729f559a1807d118f
Ryan Chaffee <rchaff7@yahoo.com>
2020-11-15 23:07:46 -0600

    Completed

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -13,8 +13,16 @@ namespace HelloASPDotNet.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
-                "<input type='text' name='name' />" +
+            string html = 
+                "<form method='post' action='/helloworld/'>" +
+                "<input type='text' name='name' placeholder='enter name' required/>" +
+                "<select name='language'>" +
+                    "<option value='english'>English</option>" +
+                    "<option value='spanish'>Spanish</option>" +
+                    "<option value='french'>French</option>" +
+                    "<option value='german'>German</option>" +
+                    "<option value='italian'>Italian</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
@@ -27,9 +35,23 @@ namespace HelloASPDotNet.Controllers
 
         [HttpGet("welcome/{name?}")]
         [HttpPost]
-        public IActionResult Welcome(string name="World")
+        public IActionResult Welcome(string name="World", string language="english")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            return Content("<h1>"+ CreateMessage(name, language) + "!</h1>", "text/html");
+        }
+
+        public static string CreateMessage(string name, string language)
+        {
+            Dictionary<string, string> greetings = new Dictionary<string, string>
+            {
+                { "english", "Hello" },
+                { "spanish", "Hola" },
+                { "french", "Bonjour" },
+                { "german", "Hallo" },
+                { "italian", "Ciao" }
+            };
+
+            return greetings[language]+" "+name;
         }
     }
 }
